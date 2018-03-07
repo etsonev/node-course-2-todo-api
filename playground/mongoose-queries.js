6ad3bf499d8a242a8abfcb0fa90fb04a1e45e0be
Emil <emiltsonev96@abv.bg>
2018-03-07 23:36:20 +0200

    Changes to mongoose-queries

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,20 +1,24 @@
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {ObjectID} = require('mongodb').ObjectID;
 
-let id = '5a96b71a10fdd481b324c98d';
+let id = '5aa058196e94c716388f9671';
 
 Todo.find({
-	text: "Test todo"
+	_id: id
 }).then((todos) => {
 	console.log('Todos', todos);
 });
 
 Todo.findOne({
-	text: "Test todo"
+	text: "Make Shake"
 }).then((todo) => {
 	console.log('Todo', todo);
 });
 
-Todo.findById(id).then((todo) => {
-	console.log('Todo', todo);
-});
+// Todo.findById(id).then((todo) => {
+// 	if(!todo) {
+// 		return console.log('Id not found');
+// 	}
+// 	console.log('Todo', todo);
+// });
